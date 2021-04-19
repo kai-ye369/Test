e7175ddf901aa7b64ca1f091cdf900e1d3b81b40
yekai <710507466@qq.com>
2021-04-19 11:21:26 +0800

    添加TestGit工程

---
+++ TestGit/TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by yekai on 2021/4/19.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
